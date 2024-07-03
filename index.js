06146fdc32d0c3722dc4bc5edfdb46321ca97f69
agilasenthil <agilas@asu.edu>
2024-07-03 01:08:58 -0700

    Updated Comments

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 //Print something in the console
-// Go to inspect and find the text in the console
-// console.log(`Hello`);
-// console.log(`I Like pizza`);
+//Go to inspect and find the text in the console
+console.log(`Hello`);
+console.log(`I Like pizza`);
 
 //Alert pop up message
-//window.alert(`This is an alert`);
+window.alert(`This is an alert`);
 
-// --> For Comments
+//--> For Comments
 
 //To display text in the screen
-// document.getElementById("myH1").textContent = `Hello`;
-// document.getElementById("myP").textContent = `How are you`;
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `How are you`;
 
 //Variables in JS
 //Declaration 
-// let x;
-// let y;
+let x;
+let y;
 
 //Assign it to the value
-// x = 10;
-// console.log(x);
+x = 10;
+console.log(x);
 
 //To understand what type of variable it is 
-// console.log(typeof x);
+console.log(typeof x);
 //Integer data type variable
-// let age = 25;
-// let price = 25.06;
+let age = 25;
+let price = 25.06;
 //To print it in the console
-// console.log(`The price is ${price}`);
+console.log(`The price is ${price}`);
 
 //String data type variable
-// let firstName = `Agila`;
+let firstName = `Agila`;
 
 //To print it in the console
-// console.log(`The name is ${firstName}`);
+console.log(`The name is ${firstName}`);
 
 //Boolean Data Type
-// let flag = false;
+let flag = false;
 
 //To print it in the screen
-// document.getElementById("myP1").textContent = `My name is ${firstName} and my online status is ${!flag}`;
-// document.getElementById("myP2").textContent = firstName;
+document.getElementById("myP1").textContent = `My name is ${firstName} and my online status is ${!flag}`;
+document.getElementById("myP2").textContent = firstName;
 
 //Accept data from the user
 //Easy Way - Window Prompt
 //Professional Way - Using HTML textBox
 
 //Window Prompt
-// let userage;
+let userage;
 
-// userage = window.prompt(`What is your age?`);
-// console.log(userage);
-// If I need age in Number then, I need to convert its type by
-// userage = Number(userage);
+userage = window.prompt(`What is your age?`);
+console.log(userage);
+//If I need age in Number then, I need to convert its type by
+userage = Number(userage);
 
 //HTML Text Box
-// let username;
-// document.getElementById("MySubmit").onclick = function(){
-//     username = document.getElementById("textEntry").value;
-//     console.log(username);
-//     document.getElementById("myH1").textContent = `Hello ${username}`;
+let username;
+document.getElementById("MySubmit").onclick = function(){
+    username = document.getElementById("textEntry").value;
+    console.log(username);
+    document.getElementById("myH1").textContent = `Hello ${username}`;
     
-// }
+}
 
 //If I declare a variable as const, its further value cannot be changed.
 
